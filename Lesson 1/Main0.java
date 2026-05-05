5ddecf2ac83862f50321d08ac5af7c18371afd3c
2012Zian <zandri.koekemoer@gmail.com>
2026-05-05 17:09:30 +0200

    lesson 1

---
+++ Lesson 1/Main0.java
@@ -0,0 +1,8 @@
+public class Main0 {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int sum = a+b;
+        System.out.println("The result is " + sum);
+    }
+}
