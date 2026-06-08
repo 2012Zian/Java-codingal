f043c742e6bc7e3fe18140aa66212800c7f361b3
2012Zian <zandri.koekemoer@gmail.com>
2026-06-08 16:49:10 +0200

    lesson 13

---
+++ Lesson_13/Main.java
@@ -0,0 +1,40 @@
+package Lesson_13;
+class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+}
+ class Student extends Person {
+    private String major;
+
+    public Student(String name, int age, String major){
+    super(name, age);
+    this.major = major;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+    Student student = new Student("Alice", 20, "Computer Science");
+    System.out.println("Name: " + student.getName());
+    System.out.println("age: " + student.getAge());
+    System.out.println("major: " + student.getMajor());
+    }
+}
