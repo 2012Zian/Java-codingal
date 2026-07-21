db605b54bcb90379c1dc224974507ae86006942c
2012Zian <zandri.koekemoer@gmail.com>
2026-07-21 16:45:12 +0200

    Lesson 24

---
+++ Lesson_24/reverseArray.java
@@ -0,0 +1,12 @@
+package Lesson_24;
+import java.util.* ;
+public class reverseArray {
+    public static void main(String[] args)
+{
+Scanner scn = new Scanner(System.in);
+System.out.print("Enter the number of elements in the list: ");
+int n = scn.nextInt();
+ArrayList<Integer> al = new ArrayList<>() ;
+System.out.println("Enter the " + n + " integers: ");                                   
+}
+}
