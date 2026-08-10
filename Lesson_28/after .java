8a6cb78c4362e2336822f2b58b26245d1adc2d1f
2012Zian <zandri.koekemoer@gmail.com>
2026-08-10 17:17:45 +0200

    lesson 28

---
+++ Lesson_28/after .java
@@ -0,0 +1,25 @@
+package Lesson_28;
+import java.util.Scanner;
+public class after  {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter the number of max elements in the array: ");
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        System.out.println("enter the " + n + "elements of the array");
+        for (int i = 0; i < n; i++) {
+        arr[i] = scn.nextInt();
+
+        }
+        displayArr(arr, 0);
+        }
+        public static void displayArr(int[] arr, int idx) {
+        if (idx == arr.length) {
+            return;
+        }
+        System.out.println(arr[idx]);
+        displayArr(arr, idx + 1);
+}
+}
+
+}
